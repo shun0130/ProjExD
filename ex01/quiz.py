a57bb60a99bd0c6f3de8a7eae619cc96cb100b6b
shun <c0a2010002@edu.teu.ac.jp>
2022-06-14 16:19:29 +0900

    あ

---
+++ ex01/quiz.py
@@ -0,0 +1,26 @@
+import random
+
+def main():
+    seikai = shutudai()
+    kaitou(seikai)
+
+def shutudai():
+    qas =  [
+            {"q":"サザエの旦那の名前は?","a":["ますお","マスオ","益男"]},
+            {"q":"カツオの妹の名前は?","a":["ワカメ","わかめ"]},
+            {"q": "タラオはカツオから見てどんな関係?","a":["甥","おい","甥っ子","おいっこ"]}
+           ]
+    print("問題:")
+    r = random.randint(0,2)
+    print(qas[r]["q"])
+    return qas[r]["a"]
+
+
+def kaitou(seikai):
+    ans = input("答えるんだ：")
+    if ans in seikai:
+        print("正解！！！")
+    else:
+        print("出直してこい")
+if __name__ == "__main__":
+    main()

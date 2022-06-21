4e9e0b2b260454ceb191ba42a0e7810e8674972e
shun <c0a2010002@edu.teu.ac.jp>
2022-06-21 16:52:02 +0900

    計算機

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,6 +1,6 @@
-import tkinter as tk
-import tkinter.messagebox as tkm
-from winreg import REG_RESOURCE_REQUIREMENTS_LIST
+import tkinter as tk #クラスtkが使えるようにインポートしている
+import tkinter.messagebox as tkm # クラスtkmが使えるようにインポートしている
+
 
 def button_click(event):
     btn = event.widget
@@ -11,17 +11,22 @@ def button_click(event):
         result = eval(eqn) #eval関数で計算する
         entry.delete(0,tk.END) #もともとあった文字を消す。
         entry.insert(tk.END,result) #計算結果を表示する
+    elif num == "C":#Cが押された時
+        s = entry.get()
+        entry.delete(len(s)-1,tk.END)
+    elif num == "AC":
+        entry.delete(0,tk.END)#ACが押された時
+
     else:
          entry.insert(tk.END,num)
-
+    
 
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.title("電卓")
-    #root.geometry("300x600")
+    root.title("電卓") #タイトルの名前をつける
 
-    entry = tk.Entry(root,justify = "right",width = 10,font=("Times New Roman",40))
+    entry = tk.Entry(root,justify = "right",width = 10,font=("Times New Roman",40)) #右側から入力されていく。
     entry.grid(row =0,column =0,columnspan=3,) #横方向に３マス結合
 
 btn = tk.Button(root,
@@ -31,7 +36,7 @@ btn = tk.Button(root,
                         )
 r, c = 1, 0 # 　r　:行番号　c　:列番号
 
-for i,num in enumerate([9,8,7,6,5,4,3,2,1,0,"+","="]):
+for i,num in enumerate([9,8,7,"/",6,5,4,"*",3,2,1,"-",0,"C","AC","="]): #記号を追加して計算出来るようにした。
     btn = tk.Button(root,
                     text=f"{num}",
                     width = 4,
@@ -42,7 +47,7 @@ for i,num in enumerate([9,8,7,6,5,4,3,2,1,0,"+","="]):
     btn.grid(row=r,column=c)
 
     c += 1
-    if (i+1)%3 ==0:
+    if (i+1)%4 ==0: #横何列で表示するかの式
         r += 1
         c = 0
 

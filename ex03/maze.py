606a8842e6e41498f8accf78618174f65de34209
shun <c0a2010002@edu.teu.ac.jp>
2022-06-28 17:53:19 +0900

    修正したよ#11

--- ex03/maze.py
+++ ex03/maze.py
@@ -7,33 +7,28 @@ def key_down(event):# key_down関数の定義
     key = event.keysym
    # print(f"{key}キーが押されました .") 
 
-def key_up(event):
+def key_up(event):# key_up関数の定義
     global key
     key = ""
 
 
     
-def main_proc():
+def main_proc():# main_proc関数の定義
     global cx, cy,mx,my
     UP,DOWN,LEFT,RIGHT =range(4)
     KEY_DIC = {"w":UP,"a":LEFT,"s":DOWN,"d":RIGHT}
     delta = {#キー：押されているキーkey/値：移動幅リスト[x,y]
-    "w"   :[0,-1],
-                           "s" :[0,+1],
-        "d":[+1,0],
-        "a":[-1,0],
-    }
-    try:
+            "w"   :[0,-1],
+            "s" :[0,+1],
+            "d":[+1,0],
+            "a":[-1,0],
+            }
+    try: # 例外処理
         if maze_bg[my+delta[key][1]][mx+delta[key][0]] == 0: #もし移動先が床なら
             my, mx = my+delta[key][1], mx+delta[key][0]
     except:
         pass
 
-    #if maze_bg[][] == 0: #もし移動先が床なら
-    #if key == "Up" : my -= 1
-    #if key == "Down" : my += 1
-    #if key == "Left" : my -= 1
-    #if key == "Right" : my += 1
     cx,cy = mx*100+50,my*100+50
     canvas.coords("tori",cx,cy)
     root.after(100,main_proc)
@@ -45,8 +40,8 @@ if __name__ == "__main__":
     canvas = tk.Canvas(root, width =1500,height=900,bg ="black")
     canvas.pack()
     maze_bg = mm.make_maze(15,9) #1:壁/0:床を表す二次元リスト
-    mm.show_maze(canvas,maze_bg) #
-   # print(maze_bg)
+    mm.show_maze(canvas,maze_bg) 
+   
 
 
     tori = tk.PhotoImage(file="fig/7.png")

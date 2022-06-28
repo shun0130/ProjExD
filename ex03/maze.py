878d785ec3505478dd8ba2c2824e9fbece9981a6
shun <c0a2010002@edu.teu.ac.jp>
2022-06-28 17:27:51 +0900

    4限追加機能実装完了

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,7 +1,8 @@
 import tkinter as tk
 import maze_maker as mm
 
-def key_down(event):
+
+def key_down(event):# key_down関数の定義
     global key
     key = event.keysym
    # print(f"{key}キーが押されました .") 
@@ -10,13 +11,17 @@ def key_up(event):
     global key
     key = ""
 
+
+    
 def main_proc():
     global cx, cy,mx,my
+    UP,DOWN,LEFT,RIGHT =range(4)
+    KEY_DIC = {"w":UP,"a":LEFT,"s":DOWN,"d":RIGHT}
     delta = {#キー：押されているキーkey/値：移動幅リスト[x,y]
-          "Up"   :[0,-1],
-          "Down" :[0,+1],
-          "Right":[+1,0],
-           "Left":[-1,0],
+    "w"   :[0,-1],
+                           "s" :[0,+1],
+        "d":[+1,0],
+        "a":[-1,0],
     }
     try:
         if maze_bg[my+delta[key][1]][mx+delta[key][0]] == 0: #もし移動先が床なら
@@ -44,7 +49,7 @@ if __name__ == "__main__":
    # print(maze_bg)
 
 
-    tori = tk.PhotoImage(file="fig/5.png")
+    tori = tk.PhotoImage(file="fig/7.png")
     mx,my = 1,1
     cx,cy = mx*100+50,my*100+50
     canvas.create_image(cx,cy, image=tori, tag="tori")

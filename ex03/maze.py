9e2d57d409df2bd1ca035800a6d500aeb4c223a3
shun <c0a2010002@edu.teu.ac.jp>
2022-06-28 15:59:44 +0900

    3限基本機能実装完了

--- ex03/maze.py
+++ ex03/maze.py
@@ -11,18 +11,27 @@ def key_up(event):
     key = ""
 
 def main_proc():
-    global cx, cy
+    global cx, cy,mx,my
     delta = {#キー：押されているキーkey/値：移動幅リスト[x,y]
-            ""     :[0,  0],
-            "Up"   :[0,-20],
-            "Down" :[0,+20],
-            "Left" :[-20,0],
-            "Right":[+20,0],
-        }
+          "Up"   :[0,-1],
+          "Down" :[0,+1],
+          "Right":[+1,0],
+           "Left":[-1,0],
+    }
+    try:
+        if maze_bg[my+delta[key][1]][mx+delta[key][0]] == 0: #もし移動先が床なら
+            my, mx = my+delta[key][1], mx+delta[key][0]
+    except:
+        pass
 
-    cx,cy = cx +delta[key][0] , cy+delta[key][1]
+    #if maze_bg[][] == 0: #もし移動先が床なら
+    #if key == "Up" : my -= 1
+    #if key == "Down" : my += 1
+    #if key == "Left" : my -= 1
+    #if key == "Right" : my += 1
+    cx,cy = mx*100+50,my*100+50
     canvas.coords("tori",cx,cy)
-    root.after(10,main_proc)
+    root.after(100,main_proc)
     
 if __name__ == "__main__":
     root =tk.Tk()
@@ -32,12 +41,13 @@ if __name__ == "__main__":
     canvas.pack()
     maze_bg = mm.make_maze(15,9) #1:壁/0:床を表す二次元リスト
     mm.show_maze(canvas,maze_bg) #
-    print(maze_bg)
+   # print(maze_bg)
 
 
     tori = tk.PhotoImage(file="fig/5.png")
-    cx,cy = 300,400
-    canvas.create_image(cx,cy,image=tori,tag="tori")
+    mx,my = 1,1
+    cx,cy = mx*100+50,my*100+50
+    canvas.create_image(cx,cy, image=tori, tag="tori")
 
     key = ""
     root.bind("<KeyPress>",key_down)
